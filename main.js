e6e827cd8fd84147ca40b3d1e47256b6ef901198
Saruul <sakub73@gmail.com>
2021-09-27 20:39:34 +1000

    final commit
    freecodecamp form validation exercise

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
+let id = (id) => document.getElementById(id);
+let classes = (classes) => document.getElementsByClassName(classes)
 
+let username = id("username");
+let email = id("email");
+let password = id("password");
+let form = id("form");
+
+let errorMsg = classes("error");
+let successIcon = classes("success-icon");
+let failureIcon = classes("failure-icon");
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    engine(username, 0, "Username cannot be blank");
+    engine(email, 1, "Email cannot be blank");
+    engine(password, 2, "Password cannot be blank");
+})
+
+let engine = (id, serial, message) => {
+    // --------using trim() method to remove whitespaces---------//
+    if (id.value.trim() === "") {
+        errorMsg[serial].innerHTML = message;
+        id.style.border = "2px solid red";
+
+        failureIcon[serial].style.opacity = "1";
+        successIcon[serial].style.opacity = "0";
+    } else {
+        errorMsg[serial].innerHTML = "";
+        id.style.border = "2px solid green";
+
+        failureIcon[serial].style.opacity = "1";
+        successIcon[serial].style.opacity = "0";
+    }
+};
